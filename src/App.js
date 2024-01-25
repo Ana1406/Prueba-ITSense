bf09feb8ec85b4cf742c680fa247708990154882
Ana1406 <anamariar317@gmail.com>
2024-01-25 17:19:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+      const [elementsList, setElementsList] = useState([
+      { type: 'fruta', name: 'Fresa' },
+      { type: 'ciudad', name: 'Armenia' },
+      {  type: 'fruta', name: 'Mango' },
+      { type: 'ciudad', name: 'Bogota' },
+      {  type: 'fruta', name: 'Ciruela' },
+      { type: 'ciudad', name: 'San Andres' },
+      {  type: 'fruta', name: 'Papaya' },
+      { type: 'fruta', name: 'Melon' },
+      { type: 'ciudad', name: 'Caldas' },
+      {  type: 'fruta', name: 'Durazno' },
+      { type: 'ciudad', name: 'Amazonas' },
+      {  type: 'fruta', name: 'Limon' },
+      { type: 'ciudad', name: 'Medellin' },
+      {  type: 'fruta', name: 'Cereza' },
+    ]);
+  const [filter,setFilter]=useState('');
+
+  const filterChange=(event)=>{
+    setFilter(event.target.value)
+  }
+
+  const filteredlist=filter?elementsList.filter(item=>item.type===filter):elementsList
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<div className=' flex flex-col  bg-slate-200/50 h-screen'> 
+      <span className='text-xs m-3'>Porfavor escoga entre 'fruta' o 'ciudad' para filtrar la lista:</span>
+      <input className= ' w-1/4 mx-3 my-2 rounded-sm text-sm  border border-b-slate-600 ' type="text" value={filter} onChange={filterChange} placeholder="Filtrar por nombre" />
+      <ul className=' list-disc list-inside text-sm space-y-1 mx-6 font-normal'> 
+        {filteredlist.map(item => (
+          <li key={item.tipo}>{item.name}</li>
+        ))}
+      </ul>
+</div>
+
   );
 }
+
 
 export default App;
